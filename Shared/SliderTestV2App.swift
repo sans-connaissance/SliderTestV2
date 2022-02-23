a4fd1e54b20420009dbfd69d354830b0b1c08366
sans-connaissance <davemalicke@gmail.com>
2022-02-22 21:59:24 -0500

    Initial Commit

---
+++ Shared/SliderTestV2App.swift
@@ -0,0 +1,17 @@
+//
+//  SliderTestV2App.swift
+//  Shared
+//
+//  Created by David Malicke on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct SliderTestV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
